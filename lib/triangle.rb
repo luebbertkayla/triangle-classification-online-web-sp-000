91b0ea9427022959197a6b5586ac44254672aafc
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-21 02:12:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,8 @@ class Triangle
   def kind 
     if (@side1 <= 0) || (@side2 <=0) || (@side3<=0)
       raise TriangleError
-    elseif (@side1 = @side2 <= @side3)
+    elseif (@side1 = @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side2 <= @side2)
+      raise TriangleError
     if (@side1 == @side2) && (@side2 == @side3)
       :equilateral
     elseif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
